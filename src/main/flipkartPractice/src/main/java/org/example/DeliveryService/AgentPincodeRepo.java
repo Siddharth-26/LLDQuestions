cdc5459543f78ed670b27db66e618e3539d2a300
Siddharth-26 <siddharthtripathi26@gmail.com>
2026-07-10 20:13:10 +0530

    Flipkart LLD Question : Flipkart Delivery Service Classes and implementation.

--- src/main/flipkartPractice/src/main/java/org/example/DeliveryService/AgentPincodeRepo.java
+++ src/main/flipkartPractice/src/main/java/org/example/DeliveryService/AgentPincodeRepo.java
@@ -1,4 +1,4 @@
-package org.example.FlipkartDeliveryService;
+package org.example.DeliveryService;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/main/flipkartPractice/src/main/java/org/example/DeliveryService/Main.java
+++ src/main/flipkartPractice/src/main/java/org/example/DeliveryService/Main.java
@@ -1,4 +1,4 @@
-package org.example.FlipkartDeliveryService;
+package org.example.DeliveryService;
 
 public class Main {
   public static void main(String[] args) {
